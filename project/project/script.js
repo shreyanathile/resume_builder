60bc6f392b7dd1505b1b3ca12ece2d7795b1d082
shreyanathile <98885642+shreyanathile@users.noreply.github.com>
2022-02-02 17:38:01 +0530

    Add files via upload

---
+++ project/project/script.js
@@ -0,0 +1,13 @@
+function validate(){
+    var username=document.getElementById("username").Value;
+    var password=document.getElementById("password").Value;
+    if(username=="admin" && password=="1234");
+    {
+        alert("Login succesfully");
+        return false;
+    }
+    
+
+        
+
+}
